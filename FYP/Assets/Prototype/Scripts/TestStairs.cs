346bebccec73ef61da0ec3713b56c0d2de3cc687
RagingWaters <rgnwtr14@gmail.com>
2019-06-27 12:46:00 +0800

    Player Controller Script and Test Scene

---
+++ FYP/Assets/Prototype/Scripts/TestStairs.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestStairs : MonoBehaviour
+{
+
+    public GameObject player;
+    public Transform Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = Target.transform.position;
+    }
+}
