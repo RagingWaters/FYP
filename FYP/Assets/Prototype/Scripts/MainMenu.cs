6f220acaa2374b7d4b574a162839780e3bec588a
Kaung Sit Hein <sithein99@gmail.com>
2019-07-09 14:40:51 +0800

    MainMenu etc with sound
    Main Menu, settings, win and lose

---
+++ FYP/Assets/Prototype/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MainMenuLoad()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Test");
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ FYP/Assets/Prototype/Scripts/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour
+{
+    private AudioSource audiSource;
+    private float musicVol = 1f;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        audiSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        audiSource.volume = musicVol;
+    }
+    public void SetVolume(float volume)
+
+    {
+        musicVol = volume;
+        Debug.Log(volume);
+    }
+}
