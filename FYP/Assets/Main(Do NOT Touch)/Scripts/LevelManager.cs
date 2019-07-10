4fde060835d273afa5f945757ed13795720f9c3e
RagingWaters <rgnwtr14@gmail.com>
2019-07-10 12:06:33 +0800

    Prototype

---
+++ FYP/Assets/Main(Do NOT Touch)/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject winscreen;
+    private static int Current;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Current = Collectible.theScore;
+        if (Current >= 4)
+        {
+            Win();
+        }
+    }
+
+    void Win()
+    {
+        winscreen.SetActive(true);
+    }
+}
