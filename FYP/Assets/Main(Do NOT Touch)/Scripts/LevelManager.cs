b77b5e3430c95207d617b064c9d75edfb0b971f1
Gc3rd4 <52161589+Gc3rd4@users.noreply.github.com>
2019-07-25 13:37:53 +0800

    cancel options

--- FYP/Assets/Main(Do NOT Touch)/Scripts/LevelManager.cs
+++ FYP/Assets/Main(Do NOT Touch)/Scripts/LevelManager.cs
@@ -23,6 +23,7 @@ public class LevelManager : MonoBehaviour
    [SerializeField] private int currentScore;
 
     public GameObject gameOverScreen;
+    public GameObject option;
 
     public Slider staminaSlider;
 
@@ -63,6 +64,7 @@ public class LevelManager : MonoBehaviour
 
     void gameOver()
     {
+        option.SetActive(false);
         newspaperText.SetActive(false);
         gameOverScreen.SetActive(true);
         staminaSlider.gameObject.SetActive(false);
