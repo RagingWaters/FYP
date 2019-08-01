29a421d8dd720f6bf9d4a64f0611d4b2236b7b6b
Kaung Sit Hein <sithein99@gmail.com>
2019-08-01 10:42:21 +0800

    3D Audio
    Made codes for falling object and 3D audio radio

---
+++ FYP/Assets/FallSound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FallSound : MonoBehaviour
+    
+{
+   
+    public AudioClip sound;
+    public float volume;
+    AudioSource source;
+    public bool alreadyPlayed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    void OnTriggerEnter()
+    {
+        if (!alreadyPlayed)
+        {
+            source.PlayOneShot(sound, volume);
+            alreadyPlayed = true;
+        }
+    }
+}
