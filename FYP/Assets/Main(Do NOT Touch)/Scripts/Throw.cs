56ac3ad2d6321a00e16d447bb8bd10b90731853e
Kaung Sit Hein <sithein99@gmail.com>
2019-10-19 23:21:02 +0800

    Changed throw script and puzzle
    New scene named Tutorial (sit) added to the game and changed up the throw script

--- FYP/Assets/Main(Do NOT Touch)/Scripts/Throw.cs
+++ FYP/Assets/Main(Do NOT Touch)/Scripts/Throw.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Throw : MonoBehaviour
 {
     public Transform player;
-    public float throwForce = 1000f;
+    public float throwForce = 10f;
     bool hasPlayer = false;
     bool beingCarried = false;
     private bool touched = false;
@@ -21,7 +21,7 @@ public class Throw : MonoBehaviour
     void Update()
     {
         float dist = Vector3.Distance(gameObject.transform.position, player.position);
-        if (dist <= 69f)
+        if (dist <= 10f)
         {
             hasPlayer = true;
             Debug.Log("in range");
@@ -33,7 +33,6 @@ public class Throw : MonoBehaviour
         }
         if ((hasPlayer && Input.GetKeyDown(KeyCode.R)) || (hasPlayer && Input.GetButtonDown("PickUp")))
         {
-            transform.position = player.transform.position;
             rb.constraints = RigidbodyConstraints.FreezeAll;
             transform.parent = player;
             beingCarried = true;
@@ -41,15 +40,15 @@ public class Throw : MonoBehaviour
         }
         if (beingCarried)
         {
-            Win();
-            /*if (touched)
+            //Win();
+            if (touched)
             {
                 rb.constraints = RigidbodyConstraints.None;
                 transform.parent = null;
                 beingCarried = false;
                 touched = false;
             }
-            if ((Input.GetKeyDown(KeyCode.T)) || Input.GetButtonDown("Throw"))
+            /*if ((Input.GetKeyDown(KeyCode.T)) || Input.GetButtonDown("Throw"))
             {
                 rb.constraints = RigidbodyConstraints.None;
                 transform.parent = null;
