e109327af74213faac7a2c321bb2ca16076447ea
Kaung Sit Hein <sithein99@gmail.com>
2019-10-22 20:08:59 +0800

    Puzzle 2, Drag and Hide
    Puzzle 2 is assembled but not 100% complete, hiding has been started, lookin to polish it and drag has been done

---
+++ FYP/Assets/Hide.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hide : MonoBehaviour
+{
+    LevelManager lm;
+    public GameObject hide;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            enemySpawner.startSpawn = false;
+            Debug.Log("ghost dieded");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            hide.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        hide.SetActive(false);
+    }
+}
--- FYP/Assets/Main(Do NOT Touch)/Scripts/Throw.cs
+++ FYP/Assets/Main(Do NOT Touch)/Scripts/Throw.cs
@@ -11,7 +11,7 @@ public class Throw : MonoBehaviour
     private bool touched = false;
     public GameObject winscreen;
     [SerializeField] Rigidbody rb;
-    public PlayerController pc;
+    public PlayerControls pc;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +22,12 @@ public class Throw : MonoBehaviour
     void Update()
     {
         float dist = Vector3.Distance(gameObject.transform.position, player.position);
-        if (dist <= 5f)
+        if (dist <= 250f)
         {
             hasPlayer = true;
             Debug.Log("in range");
+            /*rb.constraints = RigidbodyConstraints.FreezePositionX;
+            rb.constraints = RigidbodyConstraints.FreezePositionZ;*/
         }
         else
         {
@@ -37,7 +39,7 @@ public class Throw : MonoBehaviour
             rb.constraints = RigidbodyConstraints.FreezeAll;
             transform.parent = player;
             beingCarried = true;
-                pc.Speed = 0;
+                pc.currentSpeed = 100;
             Debug.Log("picked");
         }
         if (beingCarried)
@@ -52,9 +54,9 @@ public class Throw : MonoBehaviour
             }*/
             if ((Input.GetKeyDown(KeyCode.T)) || Input.GetButtonDown("Throw"))
             {
-                rb.constraints = RigidbodyConstraints.None;
+                rb.constraints = RigidbodyConstraints.FreezeAll;
                 transform.parent = null;
-                
+                pc.currentSpeed = 200;
                 Debug.Log("thrown");
             }
         }
