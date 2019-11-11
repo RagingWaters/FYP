77ce51268371309d7ef8d1038e3a1c88afa2d85f
Kaung Sit Hein <sithein99@gmail.com>
2019-11-11 12:37:40 +0800

    Pages Updated

--- FYP/Assets/Main(Do NOT Touch)/Scripts/Pages.cs
+++ FYP/Assets/Main(Do NOT Touch)/Scripts/Pages.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Pages : MonoBehaviour
 {
     public GameObject page;
-    
+    public GameObject pageGO;
     public PlayerControls pc;
     public LevelManager lm;
     public int pageValue;
@@ -53,7 +53,7 @@ public class Pages : MonoBehaviour
         Time.timeScale = 1;
         pc.enabled = true;
         page.SetActive(false);
-        
+        pageGO.SetActive(false);
         
     }
 
