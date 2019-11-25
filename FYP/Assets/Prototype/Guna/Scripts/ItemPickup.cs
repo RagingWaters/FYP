53729d163dbc546a1b24319e03770b48e5e7cad4
RagingWaters <rgnwtr14@gmail.com>
2019-11-25 20:43:41 +0800

    Inventory Part 1
    The scripts are all added to my folder

---
+++ FYP/Assets/Prototype/Guna/Scripts/ItemPickup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public Item item;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PickUp();
+        }
+    }
+
+    void PickUp()
+    {
+        Debug.Log("Picking up " + item.name);
+        bool wasPickedUp = Inventory.instance.Add(item);
+
+        if(wasPickedUp)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
