e1c8ee7e6e0b68bff940d11dfd7ff564dc760fdf
RagingWaters <rgnwtr14@gmail.com>
2019-11-25 15:33:48 +0800

    Minor change to scene
    Add stuff to object 3

---
+++ FYP/Assets/Prototype/Guna/Object3.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object3 : MonoBehaviour
+{
+    public PuzzleManager pm;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "3")
+        {
+            pm.Correct3 = true;
+        }
+
+        else
+        {
+            //this is in the wrong place message
+        }
+    }
+}
