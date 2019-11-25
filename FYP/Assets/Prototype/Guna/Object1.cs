dc3ad4f99470a2c55f00abc5d94b54114d015247
RagingWaters <rgnwtr14@gmail.com>
2019-11-25 10:57:16 +0800

    Puzzle Collider

---
+++ FYP/Assets/Prototype/Guna/Object1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object1 : MonoBehaviour
+{
+    public PuzzleManager pm;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "1")
+        {
+            pm.Correct1 = true;
+        }
+
+        else
+        {
+            //this is in the wrong place message
+        }
+    }
+}
