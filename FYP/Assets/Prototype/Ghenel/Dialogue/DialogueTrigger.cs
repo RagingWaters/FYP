f70694d853d3bab8f1097299ef073ed233b0bbb3
Gc3rd4 <52161589+Gc3rd4@users.noreply.github.com>
2019-11-26 22:10:12 +0800

    dialogue

---
+++ FYP/Assets/Prototype/Ghenel/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public GameObject dialogue1;
+    //public DialogueManager theDialogueManager;
+
+    public void TriggerDialogue()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Interact"))
+            {
+                dialogue1.SetActive(true);
+
+            }
+        
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            dialogue1.SetActive(false);
+        }
+    }
+    //void Start()
+    //{
+    //    theDialogueManager = FindObjectOfType<DialogueManager>();
+    //}
+    //void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Interact"))
+    //        {
+    //            theDialogueManager.StartDialogue(dialogue);
+    //        }
+    //    }
+    //}
+}
