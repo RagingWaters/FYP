2b869d09f343bac506507f2d1f8b4c8bf0b0bd33
RagingWaters <rgnwtr14@gmail.com>
2019-11-30 22:55:37 +0800

    Pre-Beta Update pt1
    -Flickering Light script(to be applied to affected lights)
    -Duplicated building for room cameras(WIP in separated scene)
    -Brought over some necessary things to pre-beta scene (Inventory and Instructions UI, puzzle manager)

---
+++ FYP/Assets/Prototype/Guna/Scripts/Flickering.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flickering : MonoBehaviour
+{
+    [Range(0,5)]
+    public float flickerInterval;
+    Light flashingLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashingLight = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Flashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(0, flickerInterval));
+            flashingLight.enabled = !flashingLight.enabled;
+        }
+    }
+}
