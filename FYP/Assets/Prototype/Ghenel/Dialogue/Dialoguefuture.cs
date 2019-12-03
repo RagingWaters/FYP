44abfecce165d5ec7d1b5149e7a82d597f1799a8
Gc3rd4 <52161589+Gc3rd4@users.noreply.github.com>
2019-12-03 22:26:15 +0800

    afterprebetascript

---
+++ FYP/Assets/Prototype/Ghenel/Dialogue/Dialoguefuture.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialoguefuture : MonoBehaviour
+{
+    public GameObject dialogue1;
+    public GameObject dialogue2;
+    //public DialogueManager theDialogueManager;
+    public static bool isTalking;
+    PlayerControls pc;
+    public GameObject player;
+    public Transform Target;
+    private bool cd;
+
+
+    void Start()
+    {
+        cd = false;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && isTalking == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Interact"))
+            {
+                //pc.freezeMovement = true;
+                isTalking = true;
+                dialogue1.SetActive(true);
+                StartCoroutine(ContinueDialogue());
+
+                //if (Input.GetKeyDown(KeyCode.Space) && isTalking == false || Input.GetButtonDown("Interact") && isTalking == false)
+                //{
+                //    dialogue1.SetActive(false);
+                //    dialogue2.SetActive(true);
+                //    isTalking = true;
+                //}
+            }
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //dialogue1.SetActive(false);
+            dialogue2.SetActive(false);
+            isTalking = false;
+            //pc.freezeMovement = false;
+        }
+    }
+
+    IEnumerator ContinueDialogue()
+    {
+        yield return new WaitForSeconds(7f);
+        dialogue1.SetActive(false);
+        dialogue2.SetActive(true);
+        yield return new WaitForSeconds(4f);
+        isTalking = false;
+        dialogue2.SetActive(false);
+        cd = true;
+        StartCoroutine(Teleport());
+    }
+
+    IEnumerator Teleport()
+    {
+        player.transform.position = Target.transform.position;
+        yield return new WaitForSeconds(2f);
+        cd = false;
+    }
+
+    /*public void Next()
+    {
+        dialogue1.SetActive(false);
+        dialogue2.SetActive(true);
+        
+    }*/
+    //void Start()
+    //{
+    //    //theDialogueManager = FindObjectOfType<DialogueManager>();
+    //}
+    //void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Interact"))
+    //        {
+    //            theDialogueManager.StartDialogue(dialogue);
+    //        }
+    //    }
+    //}
+}
---
+++ FYP/Assets/npc1flashback.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc1flashback : MonoBehaviour
+{
+
+    public GameObject npc1fb;
+
+    // Start is called before the first frame update
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Interact"))
+            {
+                npc1fb.SetActive(true);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            npc1fb.SetActive(false);
+        }
+    }
+}
