e2d884842839fe4c9dddd646986c50dc8e9bf2d1
RagingWaters <rgnwtr14@gmail.com>
2020-01-27 01:43:14 +0800

    Tutorial Update and Camera
    -Added a "win" to tutorial
    -Cameras are WIP for rooms

---
+++ FYP/Assets/Prototype/Ghenel/Tutorial/tutorialObject1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialObject1 : MonoBehaviour
+{
+    public tutorialwin tw;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "1")
+        {
+            tw.Correct1 = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "1")
+        {
+            tw.Correct1 = false;
+        }
+    }
+}
---
+++ FYP/Assets/Prototype/Ghenel/Tutorial/tutorialObject2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialObject2 : MonoBehaviour
+{
+    public tutorialwin tw;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "2")
+        {
+            tw.Correct2 = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "2")
+        {
+            tw.Correct2 = false;
+        }
+    }
+}
