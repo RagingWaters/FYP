f0c58676ee69c9bb2a5a68864ca36fbcdd7bbaac
RagingWaters <rgnwtr14@gmail.com>
2020-02-06 14:31:02 +0800

    Camera switch from static cam to cam follow

--- FYP/Assets/Main(Do NOT Touch)/Scripts/EnterRoom.cs
+++ FYP/Assets/Main(Do NOT Touch)/Scripts/EnterRoom.cs
@@ -5,44 +5,39 @@ using UnityEngine;
 public class EnterRoom : MonoBehaviour
 {
     public GameObject Activecam;
-    //public GameObject DisableCam;
-    
+    public CameraFollow cf;
+    public Camera testCam;
+    public static bool inRoom = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void ChangeCamPos()
     {
+        inRoom = true;
+        testCam.enabled = true;
+        Camera.main.enabled = false;
+        PlayerControls.instance.referenceCam = testCam;
         PlayerControls.instance.referenceCam.transform.position = Activecam.transform.position;
         PlayerControls.instance.referenceCam.transform.rotation = Activecam.transform.rotation;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && inRoom == false)
 
         {
             print("In room");
             ChangeCamPos();
-            //Activecam.SetActive(true);
-            //DisableCam.SetActive(false);
-            //PlayerControls.instance.referenceCam = Activecam.GetComponent<Camera>();
-            // StartCoroutine(BigBoy());  
         }
     }
-
-
-    //IEnumerator BigBoy()
-    //{
-    //    yield return new WaitForSeconds(1f);
-    //    mainCam.SetActive(false);
-    //}
 }
---
+++ FYP/Assets/Prototype/Guna/Scripts/ExitRoom.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitRoom : MonoBehaviour
+{
+    public CameraFollow cf;
+    public Camera testCam;
+    public Camera mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ChangeCamPos()
+    {
+        EnterRoom.inRoom = false;
+        mainCam.enabled = true;
+        testCam.enabled = false;
+        PlayerControls.instance.referenceCam = mainCam;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && EnterRoom.inRoom == true)
+        {
+            print("Outside room");
+            ChangeCamPos();
+        }
+    }
+}
