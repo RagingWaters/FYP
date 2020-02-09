eb3a260b4db00639186a87d159fe4377a84d9fbf
Gc3rd4 <52161589+Gc3rd4@users.noreply.github.com>
2020-02-09 21:18:21 +0800

    tutorialhints

--- FYP/Assets/pulsetrigger.cs
+++ FYP/Assets/pulsetrigger.cs
@@ -9,6 +9,7 @@ public class pulsetrigger : MonoBehaviour
     //public Animator anim;
     // Start is called before the first frame update
     public Light myLight;
+    public Light myLight1;
     public float maxIntensity = 0.7f;
     public float minIntensity = 0f;
     public float pulseSpeed = 0.5f; //here, a value of 0.5f would take 2 seconds and a value of 2f would take half a second
@@ -36,19 +37,40 @@ public class pulsetrigger : MonoBehaviour
             targetIntensity = maxIntensity;
         }
         myLight.intensity = currentIntensity;
+
+        currentIntensity = Mathf.MoveTowards(myLight1.intensity, targetIntensity, Time.deltaTime * pulseSpeed);
+        if (currentIntensity >= maxIntensity)
+        {
+            currentIntensity = maxIntensity;
+            targetIntensity = minIntensity;
+        }
+        else if (currentIntensity <= minIntensity)
+        {
+            currentIntensity = minIntensity;
+            targetIntensity = maxIntensity;
+        }
+        myLight1.intensity = currentIntensity;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            StartCoroutine(waitpulse());        }
+            StartCoroutine(waitpulse());
+
+            StartCoroutine(waitpulse2());
+        }
     }
 
     IEnumerator waitpulse()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(15f);
         box1.SetActive(true);
+    }
+
+    IEnumerator waitpulse2()
+    {
+        yield return new WaitForSeconds(25f);
         box2.SetActive(true);
     }
 }
