c013d7a2a562652f8ed1bb656547c9f85dd508df
Gc3rd4 <52161589+Gc3rd4@users.noreply.github.com>
2020-02-10 15:22:06 +0800

    anim

---
+++ FYP/Assets/scare1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scare1 : MonoBehaviour
+{
+
+    public GameObject enemy;
+
+    public Animator anim;
+
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            enemy.SetActive(true);
+            anim.Play("scare1");
+            audioSource.Play();
+            StartCoroutine(StopDialogue());
+        }
+    }
+
+    IEnumerator StopDialogue()
+    {
+        yield return new WaitForSeconds(1f);
+        enemy.SetActive(false);
+
+    }
+
+}
