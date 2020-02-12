fbbb46635266d6527cb9fdf4304f638712f291a8
Gc3rd4 <52161589+Gc3rd4@users.noreply.github.com>
2020-02-12 15:47:38 +0800

    1 day to gold

---
+++ FYP/Assets/scarezoom.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scarezoom : MonoBehaviour
+{
+    public GameObject enemy;
+    public Animator anim;
+    public AudioSource audioSource;
+    public bool alreadyPlayed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !alreadyPlayed)
+        {
+            enemy.SetActive(true);
+            anim.Play("scare2");
+            audioSource.Play();
+            alreadyPlayed = true;
+            StartCoroutine(StopDialogue());
+        }
+    }
+
+    IEnumerator StopDialogue()
+    {
+        yield return new WaitForSeconds(1f);
+        enemy.SetActive(false);
+    }
+}
